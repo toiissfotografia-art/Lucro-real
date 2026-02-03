b1348a71e3a56483c575d01e6f0d3cd9da076669
toiissfotografia-art <toiiss.fotografia@gmail.com>
2026-02-03 00:45:54 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'lucro-real-v1';
+const assets = ['./index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
